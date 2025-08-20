ffdecef9e895582578b36bdf67aec5539155a622
RudiThoeni <r.thoeni@noi.bz.it>
2025-08-20 16:51:53 +0200

    adding license headers

--- DIGIWAY/Model/DigiWayXmlModels.cs
+++ DIGIWAY/Model/DigiWayXmlModels.cs
@@ -1,4 +1,8 @@
-﻿using NetTopologySuite.Geometries;
+﻿// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using NetTopologySuite.Geometries;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
