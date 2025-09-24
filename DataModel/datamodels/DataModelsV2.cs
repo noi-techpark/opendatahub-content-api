fa9ba707b9ed25e7bec3a44b1cd2d287fbfd3184
RudiThoeni <r.thoeni@noi.bz.it>
2025-09-24 09:38:08 +0200

    remove unnecessary using directives

--- DataModel/datamodels/DataModelsV2.cs
+++ DataModel/datamodels/DataModelsV2.cs
@@ -4,12 +4,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using DataModel.Annotations;
-using RTools_NTS.Util;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace DataModel
