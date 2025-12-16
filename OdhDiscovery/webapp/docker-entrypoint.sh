2f36a08b68896610dbebe94f69dca316c44800c8
Matteo Roggia <matteo.roggia@gmail.com>
2025-12-16 14:42:09 +0100

    fix ssl nginx on chatbot rewrite

--- OdhDiscovery/webapp/docker-entrypoint.sh
+++ OdhDiscovery/webapp/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Substitute environment variables in nginx config
-# Only substitute CHATBOT_BACKEND_URL to avoid breaking nginx variables like $uri
-envsubst '${CHATBOT_BACKEND_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+# Only substitute specific vars to avoid breaking nginx variables like $uri
+envsubst '${CHATBOT_BACKEND_URL} ${CHATBOT_BACKEND_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start nginx
 exec nginx -g 'daemon off;'
