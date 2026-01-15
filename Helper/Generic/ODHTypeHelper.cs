99c87273c4fac1ba8ee711a47b668f35510f5904
Rudi Thoeni <rudi.thoeni@hotmail.com>
2026-01-15 22:35:32 +0100

    fix

--- Helper/Generic/ODHTypeHelper.cs
+++ Helper/Generic/ODHTypeHelper.cs
@@ -54,7 +54,7 @@ namespace Helper
                 "snowreport",
                 "geoshape",
                 "announcement",
-                "urbangreen"                
+                "urbangreen",                
                 "trip"
             };
         }
