11d4c52164a014fd5fde992fc25b467486fc0381
RudiThoeni <r.thoeni@noi.bz.it>
2026-04-17 16:17:27 +0200

    fixes on hgv sync, exclude rateplans comparer

--- OdhApiImporter/Helpers/LTSAPI/accommodation/LTSApiAccommodationImportHelper.cs
+++ OdhApiImporter/Helpers/LTSAPI/accommodation/LTSApiAccommodationImportHelper.cs
@@ -529,7 +529,7 @@ namespace OdhApiImporter.Helpers.LTSAPI
                     new DataInfo("accommodations", Helper.Generic.CRUDOperation.CreateAndUpdate, !opendata),
                     new EditInfo("lts.accommodations.import", importerURL),
                     new CRUDConstraints(),
-                    new CompareConfig(true, true),
+                    new CompareConfig(true, true, new List<string>() { "RatePlan", "OperationSchedule" }),  //Ignore OperationSchedule, RatePlan
                     rawdataid,
                     opendata
                 );
