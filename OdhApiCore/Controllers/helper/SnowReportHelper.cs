67e51cd7eeb095907eb84c88d084e53789e4cd3d
RudiThoeni <r.thoeni@noi.bz.it>
2026-05-28 18:04:16 +0200

    wip snowreport

--- OdhApiCore/Controllers/helper/SnowReportHelper.cs
+++ OdhApiCore/Controllers/helper/SnowReportHelper.cs
@@ -15,6 +15,84 @@ namespace OdhApiCore.Controllers.helper
 {
     public class SnowReportHelper
     {
+        public static async Task<LTSActivitySearchResult?> GetSummaries(List<string> areas, ISettings settings)
+        {
+            var ltsapi = new LtsApi(
+                settings.LtsCredentialsOpen.serviceurl,
+                settings.LtsCredentialsOpen.username,
+                settings.LtsCredentialsOpen.password,
+                settings.LtsCredentialsOpen.ltsclientid,
+                true);
+
+            //Construct the post body
+            LTSActivitySearchRequestBody body = new LTSActivitySearchRequestBody();
+            body.areaRids = areas;
+            body.onlyActive = true;
+            body.paging = new LTSAvailabilitySearchRequestPaging() { pageNumber = 1, pageSize = 25 };
+            LTSActivitySearchBodyFilterAndSummaryGroups filterandsummarygroups = new LTSActivitySearchBodyFilterAndSummaryGroups();
+
+            filterandsummarygroups.id = 0;
+            filterandsummarygroups.type = "tag";
+            filterandsummarygroups.filters.Add(new LTSActivitySearchBodyFilters()
+            {
+                id = 0,
+                isSelected = false,
+                rids = [
+                    "EB5D6F10C0CB4797A2A04818088CD6AB", // Slopes
+                    "1D273A84DBCA4709B68D295C89A003E4", // Circuit
+                    "7CA6D68BF134495F865FDD47B94320C0", // Snowpark
+                    "6285F49DBBE04393BAD29E6EF219EB03" // Other slopes
+                       ]
+            });
+
+            filterandsummarygroups.filters.Add(new LTSActivitySearchBodyFilters()
+            {
+                id = 1,
+                isSelected = false,
+                rids = [
+                    "D544A6312F8A47CF80CC4DFF8833FE50", // Cross-country ski-track
+                    "379E895958FD4693B04F5734A9CFAFAB", // Classic
+                    "E2B3F9B5B2F747A1968ECD033BED5D2B", // Skating
+                    "835EF4A6853F414DA9782607F01EEE48" // Classic and skating
+                       ]
+            });
+
+            filterandsummarygroups.filters.Add(new LTSActivitySearchBodyFilters()
+            {
+                id = 2,
+                isSelected = false,
+                rids = [
+                     "E23AA37B2AE3477F96D1C0782195AFDF", // Lifts
+                     "9CBAC00246A8467E93DD66F3A1A9C594" // Other Lifts
+                       ]
+            });
+
+            filterandsummarygroups.filters.Add(new LTSActivitySearchBodyFilters()
+            {
+                id = 3,
+                isSelected = false,
+                rids = [
+                                "F3B08D06569646F38462EDCA506D81D4", // Sledging
+                                "0BCFF98DC9DB40FDA30957791B0B2A12", //Eisbahnen
+                                "B8F8949440E340E7846BD75CED2F1F18", // Weitere Rodeln
+                                "CE6149F7BACC4FC3A4787BC706A3925A", // Schienenrodelban?
+                                "6A1CCB9E7D0D4B44A92DC748FF5991DE", //Schneebahnen
+                                "3D57411EBB42401691AC7E967AD2299A" // Tobbogan Run                                
+                            ]
+            });
+
+            body.resultSet = new LTSActivitySearchBodyResultSet()
+            {
+                filterAndSummaryGroups = new List<LTSActivitySearchBodyFilterAndSummaryGroups>() { filterandsummarygroups }
+            };
+
+            var snowreportsearch = await ltsapi.ActivitySearchRequest(null, new List<string>() { "rid" }, body);
+
+            var parsedsnowreportsearch = snowreportsearch[0].ToObject<LTSActivitySearchResult>();
+
+            return parsedsnowreportsearch;
+        }
+
         public static async Task<IEnumerable<MeasuringpointReduced>> GetMeasuringPoints(List<string> areas, ISettings settings)
         {
             var ltsapi = new LtsApi(
@@ -61,11 +139,11 @@ namespace OdhApiCore.Controllers.helper
             return measuringpointreducedlist;
         }
 
-        public static SnowReportBaseData ParseMySnowReportData(
+        public static async Task<SnowReportBaseData> ParseMySnowReportData(
             string lang,
             SkiArea skiarea,
             IEnumerable<WebcamInfo> webcams,
-            List<ODHActivityPoiLinked> snowdatalts,
+            LTSActivitySearchResult summaries,
             List<MeasuringpointReduced> measuringpoints
         )
         {
@@ -97,76 +175,52 @@ namespace OdhApiCore.Controllers.helper
                 mysnowreport.SkiMapUrl = skiarea.SkiAreaMapURL;
 
                 //TO CHECK RANDOM?
-                mysnowreport.WebcamUrl = webcams.Select(x => x.WebCamProperties.WebcamUrl).ToList();
+                mysnowreport.WebcamUrl = webcams.Select(x => x.WebCamProperties.WebcamUrl).ToList();                
 
                 //Summaries??? does not exist anymore
-                //var myliftsummary = snowdatalts.Filters.EnumCodes.Item.Where(x => x.OrderID == "1").FirstOrDefault();
+                var myliftsummary = summaries.resultSet.filterAndSummaryGroups.Select(x => x.filters.Where(x => x.id == 2).FirstOrDefault()).FirstOrDefault();
+                var myslopesummary = summaries.resultSet.filterAndSummaryGroups.Select(x => x.filters.Where(x => x.id == 0).FirstOrDefault()).FirstOrDefault();
+                var mycrosscountryskisummary = summaries.resultSet.filterAndSummaryGroups.Select(x => x.filters.Where(x => x.id == 1).FirstOrDefault()).FirstOrDefault();
+                var mysledgesummary = summaries.resultSet.filterAndSummaryGroups.Select(x => x.filters.Where(x => x.id == 3).FirstOrDefault()).FirstOrDefault();
 
-                //var myliftsummary = snowdatalts
-                //    .Filters.Tagging.Tags.FirstOrDefault()
-                //    .Item.Where(x =>
-                //        x.ItemValue.FirstOrDefault().RID == "E23AA37B2AE3477F96D1C0782195AFDF"
-                //    )
-                //    .FirstOrDefault();
+                if (myliftsummary != null)
+                {
+                    mysnowreport.openskilift =
+                        myliftsummary.quantityOpen != null
+                            ? myliftsummary.quantityOpen.ToString()
+                            : noinfotext;
+  
+                    mysnowreport.totalskilift =
+                        myliftsummary.quantity != null ? myliftsummary.quantity.ToString() : noinfotext;
+  
+                }
 
-                //activityresponse.Root.Elements("Filters").Elements("EnumCodes").Elements("Item").Where(x => x.Attribute("OrderID").Value.Equals("1")).FirstOrDefault();
-                //if (myliftsummary != null)
-                //{
-                //    mysnowreport.openskilift =
-                //        myliftsummary.CountIsOpen != null
-                //            ? myliftsummary.CountIsOpen.ToString()
-                //            : noinfotext;
-                //    //myliftsummary != null ? myliftsummary.Attribute("CountIsOpen").Value : noinfotext;
-                //    mysnowreport.totalskilift =
-                //        myliftsummary.Count != null ? myliftsummary.Count.ToString() : noinfotext;
-                //    //myliftsummary != null ? myliftsummary.Attribute("Count").Value : noinfotext;
-                //    mysnowreport.openskiliftkm =
-                //        myliftsummary.SumLenghtOpen != null
-                //            ? myliftsummary.SumLenghtOpen.ToString()
-                //            : noinfotext;
-                //    //myliftsummary != null ? myliftsummary.Attribute("SumLengthOpen").Value : noinfotext;
-                //    mysnowreport.totalskiliftkm =
-                //        myliftsummary.SumLenght != null
-                //            ? myliftsummary.SumLenght.ToString()
-                //            : noinfotext;
-                //    //myliftsummary != null ? myliftsummary.Attribute("SumLength").Value : noinfotext;
-                //}
-                
                 //Read Slope infos from Summaries
 
-                ////var mypistensummary = snowdatalts.Filters.EnumCodes.Item.Where(x => x.OrderID == "2").FirstOrDefault();
-                //var mypistensummary = snowdatalts
-                //    .Filters.Tagging.Tags.FirstOrDefault()
-                //    .Item.Where(x =>
-                //        x.ItemValue.FirstOrDefault().RID == "EB5D6F10C0CB4797A2A04818088CD6AB"
-                //    )
-                //    .FirstOrDefault();
-                ////activityresponse.Root.Elements("Filters").Elements("EnumCodes").Elements("Item").Where(x => x.Attribute("OrderID").Value.Equals("2")).FirstOrDefault();
-                //if (mypistensummary != null)
-                //{
-                //    mysnowreport.openskislopes =
-                //        mypistensummary.CountIsOpen != null
-                //            ? mypistensummary.CountIsOpen.ToString()
-                //            : noinfotext;
-                //    mysnowreport.totalskislopes =
-                //        mypistensummary.Count != null
-                //            ? mypistensummary.Count.ToString()
-                //            : noinfotext;
-                //    //mysnowreport.openskislopeskm = mypistensummary.SumLenghtOpenSpecified ? myliftsummary.SumLenghtOpen.ToString() : noinfotext;
-                //    //mysnowreport.totalskislopeskm = mypistensummary.SumLenghtSpecified ? myliftsummary.SumLenght.ToString() : noinfotext;
+                if (myslopesummary != null)
+                {
+                    mysnowreport.openskislopes =
+                        myslopesummary.quantityOpen != null
+                            ? myslopesummary.quantityOpen.ToString()
+                            : noinfotext;
+                    mysnowreport.totalskislopes =
+                        myslopesummary.quantity != null
+                            ? myslopesummary.quantity.ToString()
+                            : noinfotext;
+               
+                    //IS this info there?
+                    //double openskislopeskmdb =
+                    //    mypistensummary != null
+                    //        ? Convert.ToDouble(mypistensummary.SumLenghtOpen)
+                    //        : 0;
+                    //double tempopenskislopeskmdb = openskislopeskmdb / 1000;
+                    //mysnowreport.openskislopeskm = String.Format("{0:0}", tempopenskislopeskmdb);
 
-                //    double openskislopeskmdb =
-                //        mypistensummary != null
-                //            ? Convert.ToDouble(mypistensummary.SumLenghtOpen)
-                //            : 0;
-                //    double tempopenskislopeskmdb = openskislopeskmdb / 1000;
-                //    mysnowreport.openskislopeskm = String.Format("{0:0}", tempopenskislopeskmdb);
-
-                //    double totalskislopeskmdb =
-                //        mypistensummary != null ? Convert.ToDouble(mypistensummary.SumLenght) : 0;
-                //    double temptotalskislopeskmdb = totalskislopeskmdb / 1000;
-                //    mysnowreport.totalskislopeskm = String.Format("{0:0}", temptotalskislopeskmdb);
-                //}
+                    //double totalskislopeskmdb =
+                    //    mypistensummary != null ? Convert.ToDouble(mypistensummary.SumLenght) : 0;
+                    //double temptotalskislopeskmdb = totalskislopeskmdb / 1000;
+                    //mysnowreport.totalskislopeskm = String.Format("{0:0}", temptotalskislopeskmdb);
+                }
 
 
                 //Read Cross country skiing infos from Summaries
@@ -242,5 +296,7 @@ namespace OdhApiCore.Controllers.helper
                 return null;
             }
         }
+
+
     }
 }
